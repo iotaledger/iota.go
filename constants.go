16e1773c1142690ecff21478eb82e5234faaec18
Sascha Hanse <shanse@posteo.de>
2016-12-26 17:32:08 +0100

    adds a file for constants that are used throughout the package

---
+++ constants.go
@@ -0,0 +1,12 @@
+package giota
+
+const (
+	Radix                 = 3
+	MaxTritValue          = 1
+	MinTritValue          = -1
+	NumberOfTritsPerByte  = 5
+	NumberOfTritsPerTryte = 3
+	TryteAlphabet         = "9ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	MinTryteValue         = -13
+	MaxTryteValue         = 13
+)
