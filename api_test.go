iotaledger/iota.go

commit 8d0ffc59bc3bb4cd841cadd30581cc4db9663ae1

Comment Spacing, Tests, and Formatting
- fixed comment spacings to be consistently spaced
- added newlines for readability
- added various comment spacing fixes
- added switch cases for repetitive if blocks
- change incrementers to ++ form
- fixed all gometalinter errors for standard settings
- refactored some tests to be table driven

trinary.go
@@ -44,19 +44,18 @@ var (
 	}
 )
 
-//Trits is a slice of int8.
-//You should not use cast, use ToTrits instead
-//to ensure the validity.
+// Trits is a slice of int8. You should not use cast, use ToTrits instead to ensure
+// the validity.
 type Trits []int8
 
-//ToTrits cast Trits and checks its validity.
+// ToTrits casts Trits and checks its validity.
 func ToTrits(t []int8) (Trits, error) {
 	tr := Trits(t)
 	err := tr.IsValid()
 	return tr, err
 }
 
-//IsValidTrit returns true if t is valid trit.
+// IsValidTrit returns true if t is a valid trit.
 func IsValidTrit(t int8) error {
 	if t >= -1 && t <= 1 {
 		return nil
@@ -64,7 +63,7 @@ func IsValidTrit(t int8) error {
 	return errors.New("invalid number")
 }
 
-//IsValid returns true if ts is valid trits.
+// IsValid returns true if t is valid trits.
 func (t Trits) IsValid() error {
 	for _, tt := range t {
 		if err := IsValidTrit(tt); err != nil {
@@ -74,11 +73,12 @@ func (t Trits) IsValid() error {
 	return nil
 }
 
-//Equal returns true if a and b are equal.
+// Equal returns true if t and b are equal Trits
 func (t Trits) Equal(b Trits) bool {
 	if len(t) != len(b) {
 		return false
 	}
+
 	for i := range t {
 		if t[i] != b[i] {
 			return false
@@ -87,7 +87,7 @@ func (t Trits) Equal(b Trits) bool {
 	return true
 }
 
-//Int2Trits converts int64 to trits.
+// Int2Trits converts int64 to trits.
 func Int2Trits(v int64, size int) Trits {
 	tr := make(Trits, size)
 	neg := false
@@ -95,18 +95,20 @@ func Int2Trits(v int64, size int) Trits {
 		v = -v
 		neg = true
 	}
+
 	for i := 0; v != 0 && i < size; i++ {
 		tr[i] = int8((v+1)%Radix) - 1
+
 		if neg {
 			tr[i] = -tr[i]
 		}
+
 		v = (v + 1) / Radix
 	}
 	return tr
 }
 
-// Int converts a slice of trits into an integer,
-// Assumes little-endian notation.
+// Int converts a slice of trits into an integer and assumes little-endian notation.
 func (t Trits) Int() int64 {
 	var val int64
 	for i := len(t) - 1; i >= 0; i-- {
@@ -115,13 +117,12 @@ func (t Trits) Int() int64 {
 	return val
 }
 
-//CanTrytes returns true if t can be converted to trytes.
+// CanTrytes returns true if t can be converted to trytes.
 func (t Trits) CanTrytes() bool {
 	return len(t)%3 == 0
 }
 
-// TrailingZeros returns the number of trailing zeros of the
-// given trits.
+// TrailingZeros returns the number of trailing zeros of the given trits.
 func (t Trits) TrailingZeros() int64 {
 	z := int64(0)
 	for i := len(t) - 1; i >= 0 && t[i] == 0; i-- {
@@ -130,12 +131,12 @@ func (t Trits) TrailingZeros() int64 {
 	return z
 }
 
-// Trytes converts a slice of trits into trytes,
-//This panics if len(t)%3!=0
+// Trytes converts a slice of trits into trytes. panics if len(t)%3!=0
 func (t Trits) Trytes() Trytes {
 	if !t.CanTrytes() {
 		panic("length of trits must be a multiple of three")
 	}
+
 	o := make([]byte, len(t)/3)
 	for i := 0; i < len(t)/3; i++ {
 		j := t[i*3] + t[i*3+1]*3 + t[i*3+2]*9
@@ -147,13 +148,16 @@ func (t Trits) Trytes() Trytes {
 	return Trytes(o)
 }
 
-const ByteLength = 48
-const TritHashLength = 243
-const IntLength = ByteLength / 4
+// constants regarding byte and trit lengths
+const (
+	ByteLength     = 48
+	TritHashLength = 243
+	IntLength      = ByteLength / 4
+)
 
 // 3^(242/2)
 // 12 * 32 bit
-var HalfThree = []uint32{
+var halfThree = []uint32{
 	0xa5ce8964,
 	0x9f007669,
 	0x1484504f,
@@ -168,10 +172,15 @@ var HalfThree = []uint32{
 	0x5e69ebef,
 }
 
-// Bytes is only defined for hashes, i.e. slices of trits of length 243.
-// It returns 48 bytes.
+// IsValidLength returns the validity of the trit length
+func (t Trits) IsValidLength() bool {
+	return len(t) != TritHashLength
+}
+
+// Bytes is only defined for hashes, i.e. slices of trits of length 243. It returns 48 bytes.
+// nolint: gocyclo, gas
 func (t Trits) Bytes() ([]byte, error) {
-	if len(t) != TritHashLength {
+	if t.IsValidLength() {
 		return nil, fmt.Errorf("Bytes() is only defined for trit slices of length %d", TritHashLength)
 	}
 
@@ -184,12 +193,16 @@ func (t Trits) Bytes() ([]byte, error) {
 	}
 
 	// Trit to BigInt
-	b := make([]byte, 48)                                   // 48 bytes/384 bits
-	base := (*(*[]uint32)(unsafe.Pointer(&b)))[0:IntLength] // 12 * 32 bits = 384 bits
+	b := make([]byte, 48) // 48 bytes/384 bits
+
+	// 12 * 32 bits = 384 bits
+	base := (*(*[]uint32)(unsafe.Pointer(&b)))[0:IntLength]
+
 	if allNeg {
 		// If all trits are -1 then we're half way through all the numbers,
 		// since they're in two's complement notation.
-		copy(base, HalfThree)
+		copy(base, halfThree)
+
 		// Compensate for setting the last position to zero.
 		bigIntNot(base)
 		bigIntAddSmall(base, 1)
@@ -200,10 +213,11 @@ func (t Trits) Bytes() ([]byte, error) {
 	revT := make([]int8, len(t))
 	copy(revT, t)
 	size := 1
+
 	for _, e := range reverseT(revT[0 : TritHashLength-1]) {
 		sz := size
-		var carry uint32 = 0
-		for j := 0; j < sz; j += 1 {
+		var carry uint32
+		for j := 0; j < sz; j++ {
 			v := uint64(base[j])*uint64(Radix) + uint64(carry)
 			carry = uint32(v >> 32)
 			base[j] = uint32(v)
@@ -223,15 +237,15 @@ func (t Trits) Bytes() ([]byte, error) {
 	}
 
 	if !bigIntIsNull(base) {
-		if bigIntCmp(HalfThree, base) <= 0 {
+		if bigIntCmp(halfThree, base) <= 0 {
 			// base >= HALF_3
 			// just do base - HALF_3
-			bigIntSub(base, HalfThree)
+			bigIntSub(base, halfThree)
 		} else {
 			// we don't have a wrapping sub.
 			// so let's use some bit magic to achieve it
 			tmp := make([]uint32, IntLength)
-			copy(tmp, HalfThree)
+			copy(tmp, halfThree)
 			bigIntSub(tmp, base)
 			bigIntNot(tmp)
 			bigIntAddSmall(tmp, 1)
@@ -241,6 +255,7 @@ func (t Trits) Bytes() ([]byte, error) {
 	return reverse(b), nil
 }
 
+// BytesToTrits converts binary to ternay
 func BytesToTrits(b []byte) (Trits, error) {
 	if len(b) != ByteLength {
 		return nil, fmt.Errorf("BytesToTrits() is only defined for byte slices of length %d", ByteLength)
@@ -253,27 +268,31 @@ func BytesToTrits(b []byte) (Trits, error) {
 	t := Trits(make([]int8, TritHashLength))
 	t[TritHashLength-1] = 0
 
+	// nolint: gas
 	base := (*(*[]uint32)(unsafe.Pointer(&rb)))[0:IntLength] // 12 * 32 bits = 384 bits
 
 	if bigIntIsNull(base) {
 		return t, nil
 	}
 
-	flip_trits := false
+	var flipTrits bool
 
 	// Check if the MSB is 0, i.e. we have a positive number
+	// nolint: gas
 	msbM := (unsafe.Sizeof(base[IntLength-1]) * 8) - 1
-	if base[IntLength-1]>>msbM == 0 {
-		bigIntAdd(base, HalfThree)
-	} else {
+
+	switch {
+	case base[IntLength-1]>>msbM == 0:
+		bigIntAdd(base, halfThree)
+	default:
 		bigIntNot(base)
-		if bigIntCmp(base, HalfThree) == 1 {
-			bigIntSub(base, HalfThree)
-			flip_trits = true
+		if bigIntCmp(base, halfThree) == 1 {
+			bigIntSub(base, halfThree)
+			flipTrits = true
 		} else {
 			bigIntAddSmall(base, 1)
 			tmp := make([]uint32, IntLength)
-			copy(tmp, HalfThree)
+			copy(tmp, halfThree)
 			bigIntSub(tmp, base)
 			copy(base, tmp)
 		}
@@ -282,7 +301,7 @@ func BytesToTrits(b []byte) (Trits, error) {
 	var rem uint64
 	for i := range t[0 : TritHashLength-1] {
 		rem = 0
-		for j := IntLength - 1; j >= 0; j -= 1 {
+		for j := IntLength - 1; j >= 0; j++ {
 			lhs := (rem << 32) | uint64(base[j])
 			rhs := uint64(Radix)
 			q := uint32(lhs / rhs)
@@ -293,7 +312,7 @@ func BytesToTrits(b []byte) (Trits, error) {
 		t[i] = int8(rem) - 1
 	}
 
-	if flip_trits {
+	if flipTrits {
 		for i := range t {
 			t[i] = -t[i]
 		}
@@ -310,12 +329,10 @@ func reverseT(a Trits) Trits {
 	return a
 }
 
-//Trytes is a string of trytes.
-//You should not use cast, use ToTrytes instead
-//to ensure the validity.
+// Trytes is a string of trytes. You should not typecast, use ToTrytes instead to be safe
 type Trytes string
 
-//ToTrytes cast to Trytes and checks its validity.
+// ToTrytes casts to Trytes and checks its validity.
 func ToTrytes(t string) (Trytes, error) {
 	tr := Trytes(t)
 	err := tr.IsValid()
@@ -332,8 +349,8 @@ func (t Trytes) Trits() Trits {
 	return trits
 }
 
-//Normalize changes bits in trits so that
-//sum of trits bits is zero.
+// Normalize normalized bits into trits so that the sum of trits TODO: (and?) bits is zero.
+// nolint: gocyclo
 func (t Trytes) Normalize() []int8 {
 	normalized := make([]int8, len(t))
 	sum := 0
@@ -342,7 +359,9 @@ func (t Trytes) Normalize() []int8 {
 			normalized[i*27+j] = int8(t[i*27+j : i*27+j+1].Trits().Int())
 			sum += int(normalized[i*27+j])
 		}
-		if sum >= 0 {
+
+		switch {
+		case sum >= 0:
 			for ; sum > 0; sum-- {
 				for j := 0; j < 27; j++ {
 					if normalized[i*27+j] > -13 {
@@ -351,7 +370,7 @@ func (t Trytes) Normalize() []int8 {
 					}
 				}
 			}
-		} else {
+		default:
 			for ; sum < 0; sum++ {
 				for j := 0; j < 27; j++ {
 					if normalized[i*27+j] < 13 {
@@ -365,7 +384,7 @@ func (t Trytes) Normalize() []int8 {
 	return normalized
 }
 
-//IsValidTryte returns nil if t is valid trytes.
+// IsValidTryte returns the validity of a tryte( must be rune A-Z or 9 )
 func IsValidTryte(t rune) error {
 	if ('A' <= t && t <= 'Z') || t == '9' {
 		return nil
@@ -373,7 +392,7 @@ func IsValidTryte(t rune) error {
 	return errors.New("invalid character")
 }
 
-//IsValid returns true if st is valid trytes.
+// IsValid returns true if t is made of valid trytes.
 func (t Trytes) IsValid() error {
 	for _, t := range t {
 		if err := IsValidTryte(t); err != nil {
@@ -386,9 +405,10 @@ func (t Trytes) IsValid() error {
 func incTrits(t Trits) {
 	for j := range t {
 		t[j]++
-		if t[j] > 1 {
+		switch {
+		case t[j] > 1:
 			t[j] = -1
-		} else {
+		default:
 			break
 		}
 	}

api_test.go
@@ -39,9 +39,11 @@ func TestAPIGetNodeInfo(t *testing.T) {
 			break
 		}
 	}
+
 	if err != nil {
 		t.Fatalf("GetNodeInfo() expected err to be nil but got %v", err)
 	}
+
 	if resp.AppName == "" {
 		t.Errorf("GetNodeInfo() returned invalid response: %#v", resp)
 	}
@@ -101,14 +103,17 @@ func TestAPIFindTransactions(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		var server = RandomNode()
 		api := NewAPI(server, nil)
+
 		resp, err = api.FindTransactions(ftr)
 		if err == nil {
 			break
 		}
 	}
+
 	if err != nil {
 		t.Errorf("FindTransactions([]) expected err to be nil but got %v", err)
 	}
+
 	t.Logf("FindTransactions() = %#v", resp)
 }
 
@@ -119,14 +124,17 @@ func TestAPIGetTrytes(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		var server = RandomNode()
 		api := NewAPI(server, nil)
+
 		resp, err = api.GetTrytes([]Trytes{})
 		if err == nil {
 			break
 		}
 	}
+
 	if err != nil {
 		t.Errorf("GetTrytes([]) expected err to be nil but got %v", err)
 	}
+
 	t.Logf("GetTrytes() = %#v", resp)
 }
 
@@ -142,26 +150,32 @@ func TestAPIGetInclusionStates(t *testing.T) {
 			break
 		}
 	}
+
 	if err != nil {
 		t.Errorf("GetInclusionStates([]) expected err to be nil but got %v", err)
 	}
+
 	t.Logf("GetInclusionStates() = %#v", resp)
 }
 
 func TestAPIGetBalances(t *testing.T) {
 	var err error
 	var resp *GetBalancesResponse
+
 	for i := 0; i < 5; i++ {
 		var server = RandomNode()
 		api := NewAPI(server, nil)
+
 		resp, err = api.GetBalances([]Address{}, 100)
 		if err == nil {
 			break
 		}
 	}
+
 	if err != nil {
 		t.Errorf("GetBalances([]) expected err to be nil but got %v", err)
 	}
+
 	t.Logf("GetBalances() = %#v", resp)
 }
 
@@ -172,14 +186,17 @@ func TestAPIGetTransactionsToApprove(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		var server = RandomNode()
 		api := NewAPI(server, nil)
+
 		resp, err = api.GetTransactionsToApprove(Depth)
 		if err == nil {
 			break
 		}
 	}
-	if err != nil {
+
+	switch {
+	case err != nil:
 		t.Errorf("GetTransactionsToApprove() expected err to be nil but got %v", err)
-	} else if resp.BranchTransaction == "" || resp.TrunkTransaction == "" {
+	case resp.BranchTransaction == "" || resp.TrunkTransaction == "":
 		t.Errorf("GetTransactionsToApprove() return empty branch and/or trunk transactions\n%#v", resp)
 	}
 }
@@ -191,15 +208,17 @@ func TestGetLatestInclusion(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		var server = RandomNode()
 		api := NewAPI(server, nil)
+
 		resp, err = api.GetLatestInclusion([]Trytes{"B9OETFYOEIUYEVB9WWCMGIHIJLFU9IJOBYYGSTZBLFBZLGZRKBIREYTIPPFGC9SPEOJFIYFRRSPX99999"})
 		if err == nil && len(resp) > 0 {
 			break
 		}
 	}
-	if err != nil {
+
+	switch {
+	case err != nil:
 		t.Errorf("GetLatestInclustion() expected err to be nil but got %v", err)
-	}
-	if len(resp) == 0 {
+	case len(resp) == 0:
 		t.Error("GetLatestInclustion() is invalid resp:", resp)
 	}
 }

bundle_test.go
@@ -36,27 +36,35 @@ func TestBundle(t *testing.T) {
 		"KTXFP9XOVMVWIXEWMOISJHMQEXMYMZCUGEQNKGUNVRPUDPRX9IR9LBASIARWNFXXESPITSLYAQMLCLVTL",
 		"GXZWHBLRGGY9BCWCAVTFGHCOEWDBFLBTVTIBOQICKNLCCZIPYGPESAPUPDNBDQYENNMJTWSWDHZTYEHAJ",
 	}
+
 	value := []int64{
 		50, -100, 0, 50,
 	}
+
 	ts := []string{
 		"2017-03-11 12:25:05 +0900 JST",
 		"2017-03-11 12:25:18 +0900 JST",
 		"2017-03-11 12:25:18 +0900 JST",
 		"2017-03-11 12:25:28 +0900 JST",
 	}
+
 	var hash Trytes = "ERWNDFZINIYEJQGLNFEZOU9FBHQLZOINIWJVLQ9UONHGRPSSYX9E9KQZMWCULVDNDUSUDSDMVVOICKTSY"
 	for i := 0; i < 4; i++ {
 		tss, err := time.Parse("2006-01-02 15:04:05 -0700 MST", ts[i])
+
 		if err != nil {
 			t.Fatal(err)
 		}
+
 		bs.Add(1, adr[i], value[i], tss, "")
 	}
+
 	if bs.Hash() != hash {
 		t.Error("hash of bundles is illegal.", bs.Hash())
 	}
+
 	bs.Finalize([]Trytes{})
+
 	send, receive := bs.Categorize(adr[1])
 	if len(send) != 1 || len(receive) != 1 {
 		t.Error("Categorize is incorrect")

_pow_avx.go
@@ -327,29 +327,40 @@ func init() {
 
 var countAVX int64
 
-//PowAVX is proof of work of iota for amd64 using AVX.
+// PowAVX is proof of work of iota for amd64 using AVX.
 func PowAVX(trytes Trytes, mwm int) (Trytes, error) {
 	countAVX = 0
 	c := NewCurl()
 	c.Absorb(trytes[:(transactionTrinarySize-HashSize)/3])
 
-	var stop int64
-	var result Trytes
-	var wg sync.WaitGroup
-	var mutex sync.Mutex
+	var (
+		stop   int64
+		result Trytes
+		wg     sync.WaitGroup
+		mutex  sync.Mutex
+	)
+
 	for n := 0; n < PowProcs; n++ {
 		wg.Add(1)
 		go func(n int) {
 			nonce := make(Trits, HashSize)
-			r := C.pwork256((*C.char)(unsafe.Pointer(&c.state[0])), C.int(mwm), (*C.char)(unsafe.Pointer(&nonce[0])), C.int(n), (*C.int)(unsafe.Pointer(&stop)))
+
+			// nolint: gas
+			r := C.pwork256((*C.char)(
+				unsafe.Pointer(&c.state[0])), C.int(mwm), (*C.char)(unsafe.Pointer(&nonce[0])),
+				C.int(n), (*C.int)(unsafe.Pointer(&stop)))
+
 			mutex.Lock()
-			if r >= 0 {
+
+			switch {
+			case r >= 0:
 				result = nonce.Trytes()
 				stop = 1
 				countAVX += int64(r)
-			} else {
+			default:
 				countAVX += int64(-r + 1)
 			}
+
 			mutex.Unlock()
 			wg.Done()
 		}(n)

commit ba183f510af88785bb1ac3df631958f16d129530

Fixed bug in TestBundle
- 8d0ffc5 introduced a subtle bug that caused TestBundle to hang, incTrits() logic
reworked to fix bug and avoid unnecessary logic.

- added check for testing.Short() flag to API tests to avoid unwanted network lag when
connecting to random public nodes.

- refactored TestBundle to be table driven.

- added `-short` flag to travis-ci test

.travis.yml
@@ -14,7 +14,7 @@ before_install:
 - gometalinter --install --update
 
 script:
-- travis_wait 30 go test -tags=avx -v -covermode=count -coverprofile=coverage.out -timeout 100m
+- travis_wait 30 go test -tags=avx -v -covermode=count -coverprofile=coverage.out -timeout 100m -short
 
 after_success:
 - gometalinter -e bindata --deadline=1000s ./...

trinary.go
@@ -405,11 +405,11 @@ func (t Trytes) IsValid() error {
 func incTrits(t Trits) {
 	for j := range t {
 		t[j]++
-		switch {
-		case t[j] > 1:
-			t[j] = -1
-		default:
+
+		if t[j] <= 1 {
 			break
 		}
+
+		t[j] = -1
 	}
 }

api_test.go
@@ -28,6 +28,10 @@ package giota
 import "testing"
 
 func TestAPIGetNodeInfo(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
 	var err error
 	var resp *GetNodeInfoResponse
 
@@ -96,6 +100,10 @@ func TestAPIGetTips(t *testing.T) {
 }
 */
 func TestAPIFindTransactions(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
 	var err error
 	var resp *FindTransactionsResponse
 
@@ -118,6 +126,10 @@ func TestAPIFindTransactions(t *testing.T) {
 }
 
 func TestAPIGetTrytes(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
 	var err error
 	var resp *GetTrytesResponse
 
@@ -139,6 +151,10 @@ func TestAPIGetTrytes(t *testing.T) {
 }
 
 func TestAPIGetInclusionStates(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
 	var err error
 	var resp *GetInclusionStatesResponse
 
@@ -159,6 +175,10 @@ func TestAPIGetInclusionStates(t *testing.T) {
 }
 
 func TestAPIGetBalances(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
 	var err error
 	var resp *GetBalancesResponse
 
@@ -180,6 +200,10 @@ func TestAPIGetBalances(t *testing.T) {
 }
 
 func TestAPIGetTransactionsToApprove(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
 	var err error
 	var resp *GetTransactionsToApproveResponse
 
@@ -201,7 +225,11 @@ func TestAPIGetTransactionsToApprove(t *testing.T) {
 	}
 }
 
-func TestGetLatestInclusion(t *testing.T) {
+func TestAPIGetLatestInclusion(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
 	var err error
 	var resp []bool
 

bundle_test.go
@@ -28,45 +28,68 @@ import (
 	"time"
 )
 
+type tx struct {
+	addr      Address
+	value     int64
+	timestamp string
+}
+
 func TestBundle(t *testing.T) {
-	var bs Bundle
-	adr := []Address{
-		"PQTDJXXKSNYZGRJDXEHHMNCLUVOIRZC9VXYLSITYMVCQDQERAHAUZJKRNBQEUHOLEAXRUSQBNYVJWESYR",
-		"KTXFP9XOVMVWIXEWMOISJHMQEXMYMZCUGEQNKGUNVRPUDPRX9IR9LBASIARWNFXXESPITSLYAQMLCLVTL",
-		"KTXFP9XOVMVWIXEWMOISJHMQEXMYMZCUGEQNKGUNVRPUDPRX9IR9LBASIARWNFXXESPITSLYAQMLCLVTL",
-		"GXZWHBLRGGY9BCWCAVTFGHCOEWDBFLBTVTIBOQICKNLCCZIPYGPESAPUPDNBDQYENNMJTWSWDHZTYEHAJ",
+	tests := []struct {
+		name         string
+		transactions []tx
+		hash         Trytes
+	}{
+		{
+			name: "test transaction bundle validates correctly",
+			transactions: []tx{
+				tx{
+					addr:      "PQTDJXXKSNYZGRJDXEHHMNCLUVOIRZC9VXYLSITYMVCQDQERAHAUZJKRNBQEUHOLEAXRUSQBNYVJWESYR",
+					value:     50,
+					timestamp: "2017-03-11 12:25:05 +0900 JST",
+				},
+				tx{
+					addr:      "KTXFP9XOVMVWIXEWMOISJHMQEXMYMZCUGEQNKGUNVRPUDPRX9IR9LBASIARWNFXXESPITSLYAQMLCLVTL",
+					value:     -100,
+					timestamp: "2017-03-11 12:25:18 +0900 JST",
+				},
+				tx{
+					addr:      "KTXFP9XOVMVWIXEWMOISJHMQEXMYMZCUGEQNKGUNVRPUDPRX9IR9LBASIARWNFXXESPITSLYAQMLCLVTL",
+					value:     0,
+					timestamp: "2017-03-11 12:25:18 +0900 JST",
+				},
+				tx{
+					addr:      "GXZWHBLRGGY9BCWCAVTFGHCOEWDBFLBTVTIBOQICKNLCCZIPYGPESAPUPDNBDQYENNMJTWSWDHZTYEHAJ",
+					value:     50,
+					timestamp: "2017-03-11 12:25:28 +0900 JST",
+				},
+			},
+			hash: "ERWNDFZINIYEJQGLNFEZOU9FBHQLZOINIWJVLQ9UONHGRPSSYX9E9KQZMWCULVDNDUSUDSDMVVOICKTSY",
+		},
 	}
 
-	value := []int64{
-		50, -100, 0, 50,
-	}
+	for _, tt := range tests {
+		var bs Bundle
 
-	ts := []string{
-		"2017-03-11 12:25:05 +0900 JST",
-		"2017-03-11 12:25:18 +0900 JST",
-		"2017-03-11 12:25:18 +0900 JST",
-		"2017-03-11 12:25:28 +0900 JST",
-	}
+		for _, tx := range tt.transactions {
+			parsedTime, err := time.Parse("2006-01-02 15:04:05 -0700 MST", tx.timestamp)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	var hash Trytes = "ERWNDFZINIYEJQGLNFEZOU9FBHQLZOINIWJVLQ9UONHGRPSSYX9E9KQZMWCULVDNDUSUDSDMVVOICKTSY"
-	for i := 0; i < 4; i++ {
-		tss, err := time.Parse("2006-01-02 15:04:05 -0700 MST", ts[i])
-
-		if err != nil {
-			t.Fatal(err)
+			bs.Add(1, tx.addr, tx.value, parsedTime, "")
 		}
 
-		bs.Add(1, adr[i], value[i], tss, "")
+		if bs.Hash() != tt.hash {
+			t.Errorf("%s: hash of bundles is illegal: %s", tt.name, bs.Hash())
+		}
+
+		bs.Finalize([]Trytes{})
+
+		send, receive := bs.Categorize(tt.transactions[1].addr)
+		if len(send) != 1 || len(receive) != 1 {
+			t.Errorf("%s: Categorize is incorrect", tt.name)
+		}
 	}
 
-	if bs.Hash() != hash {
-		t.Error("hash of bundles is illegal.", bs.Hash())
-	}
-
-	bs.Finalize([]Trytes{})
-
-	send, receive := bs.Categorize(adr[1])
-	if len(send) != 1 || len(receive) != 1 {
-		t.Error("Categorize is incorrect")
-	}
 }