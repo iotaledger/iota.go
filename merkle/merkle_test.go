047839acb4e260a7c4dc5cfe4ab13be02df9dbf6
muXxer <mux3r@web.de>
2019-04-08 23:11:18 +0200

    Merkle-tree / milestone-verification (#104)
    * Made GetSpongeFunc public for use in merkle module
    
    * Added NewCurlP27 and NewCurlP81
    
    * Added merkle package for creating/validating MT
    
    * Added test cases for merkle trees
    
    * Added test case for milestone verification
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update signing/signing_milestone_test.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>
    
    * Update merkle/merkle.go
    
    Co-Authored-By: muXxer <mux3r@web.de>

---
+++ merkle/merkle_test.go
@@ -0,0 +1,53 @@
+package merkle_test
+
+import (
+	. "github.com/iotaledger/iota.go/consts"
+	. "github.com/iotaledger/iota.go/merkle"
+	. "github.com/iotaledger/iota.go/signing"
+	. "github.com/iotaledger/iota.go/trinary"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	leafCount = 32
+)
+
+var _ = Describe("Merkle", func() {
+
+	Context("Merkle()", func() {
+		It("Merkle", func() {
+			var size, depth uint64
+			var err error
+			var merkleTree, siblings, hash Trits
+
+			size = MerkleSize(leafCount)
+			Expect(size).To(Equal(uint64(63)))
+
+			depth = MerkleDepth(size)
+			Expect(depth).To(Equal(uint64(6)))
+
+			merkleTree = make(Trits, size*HashTrinarySize)
+			siblings = make(Trits, (depth-1)*HashTrinarySize)
+			hash = make(Trits, HashTrinarySize)
+
+			merkleTree, err = MerkleCreate(leafCount, "ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9ABCDEFGHIJKLMNOPQRSTUVWXYZ9", 7, SecurityLevel(2), NewCurlP81)
+			Expect(err).ToNot(HaveOccurred())
+
+			for i := uint64(0); i < leafCount; i++ {
+				siblings, err = MerkleBranch(merkleTree, siblings, size*HashTrinarySize, depth, i, leafCount)
+				Expect(err).ToNot(HaveOccurred())
+
+				merkleTreeIdx := MerkleNodeIndex(depth-1, MerkleLeafIndex(i, leafCount), depth-1) * HashTrinarySize
+
+				copy(hash, merkleTree[merkleTreeIdx:merkleTreeIdx+HashTrinarySize])
+
+				hash, err = MerkleRoot(hash, siblings, depth-1, i, NewCurlP81)
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(merkleTree[:HashTrinarySize]).To(Equal(hash))
+			}
+		})
+	})
+})
