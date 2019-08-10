120350927bf83252a81309617100157c40e2c875
Philipp Brüll <pb@simia.tech>
2019-08-10 15:33:11 +0200

    tidy dependency

--- examples/mam-transmit/main.go
+++ examples/mam-transmit/main.go
@@ -1,32 +1,24 @@
 package main
 
 import (
-	"bytes"
-	"crypto/rand"
 	"flag"
 	"fmt"
 	"log"
-	"math/big"
 
 	"github.com/simia-tech/env"
-	powsrvio "gitlab.com/powsrv.io/go/client"
-	"golang.org/x/crypto/argon2"
 
 	"github.com/iotaledger/iota.go/api"
 	"github.com/iotaledger/iota.go/consts"
 	"github.com/iotaledger/iota.go/mam/v1"
 	"github.com/iotaledger/iota.go/pow"
-	"github.com/iotaledger/iota.go/trinary"
 )
 
 var (
-	endpointURL  = env.String("ENDPOINT_URL", "https://nodes.thetangle.org:443")
-	powSrvAPIKey = env.String("POWSRV_API_KEY", "")
-	seed         = env.String("SEED", "")
-	seedPassword = env.String("SEED_PASSWORD", "")
-	mwm          = env.Int("MWM", 9)
-	mode         = env.String("MODE", "public", env.AllowedValues("public", "private", "restricted"))
-	sideKey      = env.String("SIDE_KEY", "")
+	endpointURL = env.String("ENDPOINT_URL", "https://nodes.thetangle.org:443")
+	seed        = env.String("SEED", "")
+	mwm         = env.Int("MWM", 9)
+	mode        = env.String("MODE", "public", env.AllowedValues("public", "private", "restricted"))
+	sideKey     = env.String("SIDE_KEY", "")
 )
 
 func main() {
@@ -38,20 +30,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	powFunc := pow.ProofOfWorkFunc(nil)
-	if apiKey := powSrvAPIKey.Get(); apiKey == "" {
-		_, powFunc = pow.GetFastestProofOfWorkImpl()
-	} else {
-		powClient := &powsrvio.PowClient{
-			ReadTimeOutMs: 5000,
-			APIKey:        apiKey,
-			Verbose:       true,
-		}
-		if err := powClient.Init(); err != nil {
-			log.Fatal(err)
-		}
-		powFunc = powClient.PowFunc
-	}
+	_, powFunc := pow.GetFastestProofOfWorkImpl()
 
 	api, err := api.ComposeAPI(api.HTTPClientSettings{
 		URI:                  endpointURL.Get(),
@@ -61,7 +40,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	transmitter := mam.NewTransmitter(api, seedFromEnv(), uint64(mwm.Get()), consts.SecurityLevelMedium)
+	transmitter := mam.NewTransmitter(api, seed.Get(), uint64(mwm.Get()), consts.SecurityLevelMedium)
 	if err := transmitter.SetMode(cm, sideKey.Get()); err != nil {
 		log.Fatal(err)
 	}
@@ -74,42 +53,4 @@ func main() {
 		}
 		fmt.Printf("transmitted to root %q\n", root)
 	}
-}
-
-func seedFromEnv() trinary.Trytes {
-	if v := seed.Get(); v != "" {
-		return v
-	}
-	if v := seedPassword.Get(); v != "" {
-		password := bytes.TrimSpace([]byte(v))
-		seedBytes := argon2.IDKey(password, []byte(""), 50, 64*1024, 4, 48)
-		return toTrytes(seedBytes)
-	}
-	seedBytes := make([]byte, 48)
-	rand.Read(seedBytes)
-	seed := toTrytes(seedBytes)
-	fmt.Printf("generated random seed %s\n", seed)
-	return seed
-}
-
-func toTrytes(b []byte) trinary.Trytes {
-	b = []byte(new(big.Int).SetBytes(b).Text(27))
-	index := 0
-	for range b {
-		switch {
-		case b[index] == 48:
-			b[index] = 57
-			break
-		case b[index] > 48 && b[index] < 58:
-			b[index] += 16
-			break
-		case b[index] > 96 && b[index] < 123:
-			b[index] -= 23
-			break
-		default:
-			b[index] += 9
-		}
-		index++
-	}
-	return trinary.Trytes(b)
 }
--- go.mod
+++ go.mod
@@ -4,24 +4,24 @@ go 1.12
 
 require (
 	github.com/AndreasBriese/bbloom v0.0.0-20190306092124-e2d15f34fcf9 // indirect
-	github.com/apsdehal/go-logger v0.0.0-20190506062552-f85330a4b532 // indirect
 	github.com/beevik/ntp v0.2.0
 	github.com/cespare/xxhash v1.1.0
 	github.com/dgraph-io/badger v1.5.4
 	github.com/dgryski/go-farm v0.0.0-20190323231341-8198c7b169ec // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.2.0 // indirect
 	github.com/onsi/ginkgo v1.8.0
 	github.com/onsi/gomega v1.5.0
 	github.com/pkg/errors v0.8.1
 	github.com/simia-tech/env v0.1.0
 	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/tidwall/pretty v1.0.0 // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
-	gitlab.com/powsrv.io/go/api v0.0.0-20180825222338-acd4e4dcc0bf // indirect
-	gitlab.com/powsrv.io/go/client v0.0.0-20190717084126-be3e20edad49
 	go.mongodb.org/mongo-driver v1.0.0
-	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5
-	google.golang.org/grpc v1.22.1 // indirect
+	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5 // indirect
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
 	gopkg.in/h2non/gock.v1 v1.0.14
 )
