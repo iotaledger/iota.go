bd4a28ffa7f0063d094d9fd95e8068361aded822
Andrea Villa <andreakarimodm@gmail.com>
2020-06-03 13:03:52 +0200

    merkle_calculator: spaces converted to tabs to respect code style

--- tools/merkle_calculator/merkle_tree_calculator.go
+++ tools/merkle_calculator/merkle_tree_calculator.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-  "fmt"
-  "time"
-  "flag"
+	"flag"
+	"fmt"
+	"time"
 
-  "github.com/iotaledger/iota.go/trinary"
-  "github.com/iotaledger/iota.go/merkle"
+	"github.com/iotaledger/iota.go/merkle"
+	"github.com/iotaledger/iota.go/trinary"
 )
 
 // createMerkleTreeFile calculates a merkle tree and persists it into a file.
 func createMerkleTreeFile(filePath string, seed trinary.Hash, securityLvl int, depth int) error {
 
-  mt := merkle.CreateMerkleTree(seed, securityLvl, depth)
+	mt := merkle.CreateMerkleTree(seed, securityLvl, depth)
 
-  if err := merkle.StoreMerkleTreeFile(filePath, mt); err != nil {
-    return err
-  }
+	if err := merkle.StoreMerkleTreeFile(filePath, mt); err != nil {
+		return err
+	}
 
-  fmt.Printf("Merkle tree root: %v\n", mt.Root)
+	fmt.Printf("Merkle tree root: %v\n", mt.Root)
 
-  return nil
+	return nil
 }
 
 func main() {
 
-  var depth int
-  var securityLevel int
-  var seed string
-  var outputPath string
+	var depth int
+	var securityLevel int
+	var seed string
+	var outputPath string
 
-  flag.IntVar(&depth, "depth", 0, "Depth of the Merkle tree to create")
-  flag.IntVar(&securityLevel, "securityLevel", 0, "Security level of the private key used")
-  flag.StringVar(&seed, "seed", "", "Seed for leaves derivation")
-  flag.StringVar(&outputPath, "output", "", "Output file path")
+	flag.IntVar(&depth, "depth", 0, "Depth of the Merkle tree to create")
+	flag.IntVar(&securityLevel, "securityLevel", 0, "Security level of the private key used")
+	flag.StringVar(&seed, "seed", "", "Seed for leaves derivation")
+	flag.StringVar(&outputPath, "output", "", "Output file path")
 
-  flag.Parse()
+	flag.Parse()
 
-  if depth < 1 {
-    fmt.Println("'depth' cannot be lower than 1")
-    return
-  }
+	if depth < 1 {
+		fmt.Println("'depth' cannot be lower than 1")
+		return
+	}
 
-  if securityLevel < 1 || securityLevel > 3 {
-    fmt.Println("'securityLevel' must be 1, 2 or 3")
-    return
-  }
+	if securityLevel < 1 || securityLevel > 3 {
+		fmt.Println("'securityLevel' must be 1, 2 or 3")
+		return
+	}
 
-  if len(seed) != 81 {
-    fmt.Println("'seed' must be a string of 81 trytes")
-    return
-  }
+	if len(seed) != 81 {
+		fmt.Println("'seed' must be a string of 81 trytes")
+		return
+	}
 
-  if outputPath == "" {
-    fmt.Println("'output' is required")
-    return
-  }
+	if outputPath == "" {
+		fmt.Println("'output' is required")
+		return
+	}
 
-  fmt.Printf("calculating %d addresses...\n", 1 << uint(depth))
+	fmt.Printf("calculating %d addresses...\n", 1<<uint(depth))
 
-  ts := time.Now()
+	ts := time.Now()
 
-  createMerkleTreeFile(outputPath, trinary.Hash(seed), securityLevel, depth)
+	createMerkleTreeFile(outputPath, trinary.Hash(seed), securityLevel, depth)
 
-  fmt.Printf("Took %v seconds.\n", time.Since(ts).Truncate(time.Second))
+	fmt.Printf("Took %v seconds.\n", time.Since(ts).Truncate(time.Second))
 }
