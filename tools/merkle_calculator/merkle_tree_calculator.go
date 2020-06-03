399e7f1fad55935ef77f23007629ab56106f5d85
Andrea Villa <andreakarimodm@gmail.com>
2020-06-03 13:50:47 +0200

    merkle_calculator: use log package instead of fmt

--- tools/merkle_calculator/merkle_tree_calculator.go
+++ tools/merkle_calculator/merkle_tree_calculator.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 	"time"
 
 	"github.com/iotaledger/iota.go/merkle"
@@ -24,38 +24,38 @@ func main() {
 	flag.Parse()
 
 	if depth < 1 {
-		fmt.Println("'depth' cannot be lower than 1")
+		log.Println("'depth' cannot be lower than 1")
 		return
 	}
 
 	if securityLevel < 1 || securityLevel > 3 {
-		fmt.Println("'securityLevel' must be 1, 2 or 3")
+		log.Println("'securityLevel' must be 1, 2 or 3")
 		return
 	}
 
 	if len(seed) != 81 {
-		fmt.Println("'seed' must be a string of 81 trytes")
+		log.Println("'seed' must be a string of 81 trytes")
 		return
 	}
 
 	if outputPath == "" {
-		fmt.Println("'output' is required")
+		log.Println("'output' is required")
 		return
 	}
 
-	fmt.Printf("calculating %d addresses...\n", 1<<uint(depth))
+	log.Printf("calculating %d addresses...\n", 1<<uint(depth))
 
 	ts := time.Now()
 
 	mt := merkle.CreateMerkleTree(trinary.Hash(seed), securityLevel, depth)
 
 	if err := merkle.StoreMerkleTreeFile(outputPath, mt); err != nil {
-		fmt.Println("Error persisting Merkle tree: %v", err)
+		log.Panicf("Error persisting Merkle tree: %v", err)
 		return
 	}
 
-	fmt.Printf("Merkle tree root: %v\n", mt.Root)
+	log.Printf("Merkle tree root: %v\n", mt.Root)
 
-	fmt.Printf("Took %v seconds.\n", time.Since(ts).Truncate(time.Second))
+	log.Printf("Took %v seconds.\n", time.Since(ts).Truncate(time.Second))
 
 }
