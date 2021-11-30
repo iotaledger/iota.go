7f30aef370631b19c95942edf8a106a14f6c4924
Wolfgang Welz <welzwo@gmail.com>
2021-11-30 12:17:07 +0100

    Add test vectors from RFC-28 (#297)
    * add test vectors from RFC-28
    
    * fix the tests for go 1.15
    
    * make the test struct consistent with other tests

--- signature_test.go
+++ signature_test.go
@@ -1,12 +1,23 @@
 package iotago_test
 
 import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
-	"github.com/iotaledger/iota.go/v3/tpkg"
-
+	"github.com/iotaledger/hive.go/serializer/v2"
 	"github.com/iotaledger/iota.go/v3"
+	"github.com/iotaledger/iota.go/v3/tpkg"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestSignatureSelector(t *testing.T) {
+	_, err := iotago.SignatureSelector(100)
+	assert.True(t, errors.Is(err, iotago.ErrUnknownSignatureType))
+}
 
 func TestEd25519Signature_DeSerialize(t *testing.T) {
 	tests := []deSerializeTest{
@@ -19,5 +30,41 @@ func TestEd25519Signature_DeSerialize(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, tt.deSerialize)
+	}
+}
+
+func TestEd25519Signature_Valid(t *testing.T) {
+	type test struct {
+		Address   tpkg.HexBytes `json:"address"`
+		Message   tpkg.HexBytes `json:"message"`
+		PublicKey tpkg.HexBytes `json:"pub_key"`
+		Signature tpkg.HexBytes `json:"signature"`
+		Valid     bool          `json:"valid"`
+	}
+	var tests []test
+	// load the tests from file
+	b, err := ioutil.ReadFile(filepath.Join("testdata", t.Name()+".json"))
+	require.NoError(t, err)
+	require.NoError(t, json.Unmarshal(b, &tests))
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			// deserialize the address from the test
+			addr := &iotago.Ed25519Address{}
+			_, err = addr.Deserialize(tt.Address, serializer.DeSeriModePerformValidation, nil)
+			require.NoError(t, err)
+			// create the signature type
+			sig := &iotago.Ed25519Signature{}
+			copy(sig.PublicKey[:], tt.PublicKey)
+			copy(sig.Signature[:], tt.Signature)
+
+			sigError := sig.Valid(tt.Message, addr)
+			switch tt.Valid {
+			case true:
+				assert.NoError(t, sigError)
+			case false:
+				assert.Error(t, sigError)
+			}
+		})
 	}
 }
