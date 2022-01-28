2d3efcbe04c5ad056c26a5c5e294521c47d9f7c2
Luca Moser <moser.luca@gmail.com>
2022-01-28 13:51:20 +0100

    re-add fuzzing folder

---
+++ fuzzing/message/message.go
@@ -0,0 +1,24 @@
+package message
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/iotaledger/hive.go/serializer"
+	"github.com/iotaledger/iota.go/v2"
+)
+
+func Fuzz(data []byte) int {
+	m := &iotago.Message{}
+	if _, err := m.Deserialize(data, serializer.DeSeriModePerformValidation); err != nil {
+		return 0
+	}
+	seriData, err := m.Serialize(serializer.DeSeriModePerformValidation)
+	if err != nil {
+		panic(fmt.Sprintf("should be able to serialize: %q", err))
+	}
+	if !bytes.Equal(data[:len(seriData)], seriData) {
+		panic("data from serialization should be same as origin")
+	}
+	return 1
+}
