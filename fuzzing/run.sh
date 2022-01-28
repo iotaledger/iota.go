25ba8d2d279ad03fba0e23cd03d23f63db3885da
Luca Moser <moser.luca@gmail.com>
2022-01-28 15:07:21 +0100

    remove legacy fuzzing (#306)

--- fuzzing/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "install go-fuzz..."
-go get -u github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
-chmod -R 777 ./fuzzing
-cd ./fuzzing/message || exit
-echo "go-fuzz-build..."
-go-fuzz-build
-echo "go-fuzz..."
-go-fuzz
