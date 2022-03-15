354a904c990c0c9b7c4fee53ff81e032388d559a
Luca Moser <me@lucamoser.ch>
2022-03-15 15:39:08 +0100

    Use newer native token validation rules with minted/melted fields  (#322)
    * implements preliminarly updated native token validation rules
    
    * allow mint on foundry genesis
    
    * adds additional syntactical foundry checks

--- transaction_essence.go
+++ transaction_essence.go
@@ -43,8 +43,8 @@ var (
 	ErrNFTOutputCyclicAddress = errors.New("nft output's NFTID corresponds to address field")
 	// ErrFoundryOutputInvalidMaximumSupply gets returned when a FoundryOutput's MaximumSupply is invalid.
 	ErrFoundryOutputInvalidMaximumSupply = errors.New("foundry output's maximum supply is invalid")
-	// ErrFoundryOutputInvalidCirculatingSupply gets returned when a FoundryOutput's CirculatingSupply is invalid.
-	ErrFoundryOutputInvalidCirculatingSupply = errors.New("foundry output's circulating supply is invalid")
+	// ErrFoundryOutputInvalidMintedMeltedTokens gets returned when a FoundryOutput's minted is invalid.
+	ErrFoundryOutputInvalidMintedMeltedTokens = errors.New("foundry output's minted/melted tokens counters are invalid")
 	// ErrOutputsSumExceedsTotalSupply gets returned if the sum of the output deposits exceeds the total supply of tokens.
 	ErrOutputsSumExceedsTotalSupply = errors.New("accumulated output balance exceeds total supply")
 	// ErrOutputDepositsMoreThanTotalSupply gets returned if an output deposits more than the total supply.
