025145006313d0873e526d3c90edd9b78d169c90
Alexander Sporn <github@alexsporn.de>
2022-04-26 13:26:06 +0200

    - Add json annotations to ProtocolParameters (#347)
    - Added a NetworkID function to ProtocolParameters

--- protocol.go
+++ protocol.go
@@ -24,15 +24,19 @@ func NetworkIDFromString(networkIDStr string) NetworkID {
 // ProtocolParameters defines the parameters of the protocol.
 type ProtocolParameters struct {
 	// The version of the protocol running.
-	Version byte
+	Version byte `json:"version"`
 	// The human friendly name of the network.
-	NetworkName string
+	NetworkName string `json:"networkName"`
 	// The HRP prefix used for Bech32 addresses in the network.
-	Bech32HRP string
+	Bech32HRP NetworkPrefix `json:"bech32HRP"`
 	// The minimum pow score of the network.
-	MinPowScore float64
+	MinPowScore float64 `json:"minPowScore"`
 	// The rent structure used by given node/network.
-	RentStructure RentStructure
+	RentStructure RentStructure `json:"rentStructure"`
 	// TokenSupply defines the current token supply on the network.
-	TokenSupply uint64
+	TokenSupply uint64 `json:"tokenSupply"`
+}
+
+func (p ProtocolParameters) NetworkID() NetworkID {
+	return NetworkIDFromString(p.NetworkName)
 }
