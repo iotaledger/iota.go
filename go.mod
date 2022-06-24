946b36ac8ab613fcd5c12999f3013af437f67712
muXxer <git@muxxer.de>
2022-06-24 12:35:44 +0200

    Move signingprovider from inx-coordinator to iota.go (#392)

--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	filippo.io/edwards25519 v1.0.0
 	github.com/eclipse/paho.mqtt.golang v1.4.1
 	github.com/ethereum/go-ethereum v1.10.19
-	github.com/iotaledger/hive.go/serializer/v2 v2.0.0-20220531132324-8347a155e220
+	github.com/iotaledger/hive.go/serializer/v2 v2.0.0-20220623193802-86a373581cc1
 	github.com/iotaledger/iota.go v1.0.0
 	github.com/pasztorpisti/qs v0.0.0-20171216220353-8d6c33ee906c
-	github.com/stretchr/testify v1.7.3
-	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.7.4
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
 	google.golang.org/grpc v1.47.0
 	google.golang.org/protobuf v1.28.0
 	gopkg.in/h2non/gock.v1 v1.1.2
@@ -25,15 +26,14 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
-	golang.org/x/net v0.0.0-20220617184016-355a448f1bc9 // indirect
+	golang.org/x/net v0.0.0-20220622184535-263ec571b305 // indirect
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
-	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+	golang.org/x/sys v0.0.0-20220622161953-175b2fd9d664 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
-	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
+	google.golang.org/genproto v0.0.0-20220623142657-077d458a5694 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ signingprovider/signing_provider_test.go
@@ -0,0 +1,151 @@
+package signingprovider_test
+
+import (
+	"crypto/ed25519"
+	"errors"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	iotago "github.com/iotaledger/iota.go/v3"
+	"github.com/iotaledger/iota.go/v3/keymanager"
+	"github.com/iotaledger/iota.go/v3/signingprovider"
+	"github.com/iotaledger/iota.go/v3/tpkg"
+)
+
+func TestInMemoryEd25519MilestoneSignerProvider(t *testing.T) {
+
+	privateKeys := make([]ed25519.PrivateKey, 0)
+
+	pubKey1, privKey1, err := ed25519.GenerateKey(nil)
+	assert.NoError(t, err)
+	privateKeys = append(privateKeys, privKey1)
+
+	pubKey2, privKey2, err := ed25519.GenerateKey(nil)
+	assert.NoError(t, err)
+	privateKeys = append(privateKeys, privKey2)
+
+	pubKey3, privKey3, err := ed25519.GenerateKey(nil)
+	assert.NoError(t, err)
+	privateKeys = append(privateKeys, privKey3)
+
+	pubKey4, privKey4, err := ed25519.GenerateKey(nil)
+	assert.NoError(t, err)
+	privateKeys = append(privateKeys, privKey4)
+
+	var msPubKey1 iotago.MilestonePublicKey
+	copy(msPubKey1[:], pubKey1)
+
+	var msPubKey2 iotago.MilestonePublicKey
+	copy(msPubKey2[:], pubKey2)
+
+	var msPubKey3 iotago.MilestonePublicKey
+	copy(msPubKey3[:], pubKey3)
+
+	var msPubKey4 iotago.MilestonePublicKey
+	copy(msPubKey4[:], pubKey4)
+
+	km := keymanager.New()
+	km.AddKeyRange(pubKey1, 0, 9)
+	km.AddKeyRange(pubKey2, 3, 10)
+	km.AddKeyRange(pubKey3, 8, 15)
+	km.AddKeyRange(pubKey4, 17, 19)
+
+	signer := signingprovider.NewInMemoryEd25519MilestoneSignerProvider(privateKeys, km, 2)
+	require.Equal(t, 2, signer.PublicKeysCount())
+
+	type test struct {
+		name            string
+		ms              *iotago.Milestone
+		signingErr      error
+		verificationErr error
+	}
+
+	tests := []test{
+		func() test {
+			msPayload := &iotago.Milestone{
+				Parents:           tpkg.SortedRandBlockIDs(1 + rand.Intn(7)),
+				Index:             4,
+				Timestamp:         uint32(time.Now().Unix()),
+				AppliedMerkleRoot: tpkg.Rand32ByteArray(),
+			}
+
+			return test{
+				name:            "ok - 2 of 2 from applicable set",
+				ms:              msPayload,
+				signingErr:      nil,
+				verificationErr: nil,
+			}
+		}(),
+		func() test {
+			msPayload := &iotago.Milestone{
+				Parents:           tpkg.SortedRandBlockIDs(1 + rand.Intn(7)),
+				Index:             8,
+				Timestamp:         uint32(time.Now().Unix()),
+				AppliedMerkleRoot: tpkg.Rand32ByteArray(),
+			}
+
+			return test{
+				name:            "ok - 2 of 3 from applicable set",
+				ms:              msPayload,
+				signingErr:      nil,
+				verificationErr: nil,
+			}
+		}(),
+		func() test {
+			msPayload := &iotago.Milestone{
+				Parents:           tpkg.SortedRandBlockIDs(1 + rand.Intn(7)),
+				Index:             20,
+				Timestamp:         uint32(time.Now().Unix()),
+				AppliedMerkleRoot: tpkg.Rand32ByteArray(),
+			}
+
+			return test{
+				name:            "err - too few signatures for signing",
+				ms:              msPayload,
+				signingErr:      iotago.ErrMilestoneTooFewSignatures,
+				verificationErr: nil,
+			}
+		}(),
+		func() test {
+			msPayload := &iotago.Milestone{
+				Parents:           tpkg.SortedRandBlockIDs(1 + rand.Intn(7)),
+				Index:             17,
+				Timestamp:         uint32(time.Now().Unix()),
+				AppliedMerkleRoot: tpkg.Rand32ByteArray(),
+			}
+
+			return test{
+				name:            "err - too few signatures for verification",
+				ms:              msPayload,
+				signingErr:      nil,
+				verificationErr: iotago.ErrMilestoneTooFewSignaturesForVerificationThreshold,
+			}
+		}(),
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			indexSigner := signer.MilestoneIndexSigner(tt.ms.Index)
+
+			err := tt.ms.Sign(indexSigner.PublicKeys(), indexSigner.SigningFunc())
+			if tt.signingErr != nil {
+				assert.True(t, errors.Is(err, tt.signingErr))
+				return
+			}
+			assert.NoError(t, err)
+
+			err = tt.ms.VerifySignatures(signer.PublicKeysCount(), indexSigner.PublicKeysSet())
+			if tt.verificationErr != nil {
+				println(err.Error())
+				assert.True(t, errors.Is(err, tt.verificationErr))
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
