cb8ef79b9fe0410d54f3eb4162bf2a32ea4926d1
muXxer <git@muxxer.de>
2022-07-20 18:01:21 +0200

    Add test cases for MaxInputsCount, MaxOutputsCount and RefUTXOIndexMax

--- transaction_test.go
+++ transaction_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/iotaledger/hive.go/serializer/v2"
 	iotago "github.com/iotaledger/iota.go/v3"
 	"github.com/iotaledger/iota.go/v3/tpkg"
 )
@@ -17,6 +18,82 @@ func TestTransactionDeSerialize(t *testing.T) {
 			name:   "ok",
 			source: tpkg.RandTransaction(),
 			target: &iotago.Transaction{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, tt.deSerialize)
+	}
+}
+
+func TestTransactionDeSerialize_MaxInputsCount(t *testing.T) {
+	tests := []deSerializeTest{
+		{
+			name:      "ok",
+			source:    tpkg.RandTransactionWithInputCount(iotago.MaxInputsCount),
+			target:    &iotago.Transaction{},
+			seriErr:   nil,
+			deSeriErr: nil,
+		},
+		{
+			name:      "too many inputs",
+			source:    tpkg.RandTransactionWithInputCount(iotago.MaxInputsCount + 1),
+			target:    &iotago.Transaction{},
+			seriErr:   serializer.ErrArrayValidationMaxElementsExceeded,
+			deSeriErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, tt.deSerialize)
+	}
+}
+
+func TestTransactionDeSerialize_MaxOutputsCount(t *testing.T) {
+	tests := []deSerializeTest{
+		{
+			name:      "ok",
+			source:    tpkg.RandTransactionWithOutputCount(iotago.MaxOutputsCount),
+			target:    &iotago.Transaction{},
+			seriErr:   nil,
+			deSeriErr: nil,
+		},
+		{
+			name:      "too many outputs",
+			source:    tpkg.RandTransactionWithOutputCount(iotago.MaxOutputsCount + 1),
+			target:    &iotago.Transaction{},
+			seriErr:   serializer.ErrArrayValidationMaxElementsExceeded,
+			deSeriErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, tt.deSerialize)
+	}
+}
+
+func TestTransactionDeSerialize_RefUTXOIndexMax(t *testing.T) {
+	tests := []deSerializeTest{
+		{
+			name: "ok",
+			source: tpkg.RandTransactionWithEssence(tpkg.RandTransactionEssenceWithInputs(iotago.Inputs{
+				&iotago.UTXOInput{
+					TransactionID:          tpkg.RandTransactionID(),
+					TransactionOutputIndex: iotago.RefUTXOIndexMax,
+				},
+			})),
+			target:    &iotago.Transaction{},
+			seriErr:   nil,
+			deSeriErr: nil,
+		},
+		{
+			name: "wrong ref index",
+			source: tpkg.RandTransactionWithEssence(tpkg.RandTransactionEssenceWithInputs(iotago.Inputs{
+				&iotago.UTXOInput{
+					TransactionID:          tpkg.RandTransactionID(),
+					TransactionOutputIndex: iotago.RefUTXOIndexMax + 1,
+				},
+			})),
+			target:    &iotago.Transaction{},
+			seriErr:   iotago.ErrRefUTXOIndexInvalid,
+			deSeriErr: nil,
 		},
 	}
 	for _, tt := range tests {
