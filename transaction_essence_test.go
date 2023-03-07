3d7db67f47eb005ba23af786711ea66c0807430e
Alexander Sporn <github@alexsporn.de>
2023-03-07 08:21:31 +0100

    Added foundry test case
    # Conflicts:
    #	transaction_test.go

--- transaction_essence_test.go
+++ transaction_essence_test.go
@@ -2,6 +2,7 @@ package iotago_test
 
 import (
 	"errors"
+	"math/big"
 	"testing"
 
 	"github.com/iotaledger/iota.go/v4/tpkg"
@@ -37,10 +38,9 @@ func TestChainConstrainedOutputUniqueness(t *testing.T) {
 
 	aliasAddress := iotago.AliasAddressFromOutputID(inputIDs[0])
 	aliasID := aliasAddress.AliasID()
+
 	nftAddress := iotago.NFTAddressFromOutputID(inputIDs[0])
 	nftID := nftAddress.NFTID()
-
-	// TODO: add a foundy testcase
 
 	tests := []deSerializeTest{
 		{
@@ -94,6 +94,56 @@ func TestChainConstrainedOutputUniqueness(t *testing.T) {
 						NFTID:  nftID,
 						Conditions: iotago.NFTOutputUnlockConditions{
 							&iotago.AddressUnlockCondition{Address: ident1},
+						},
+						Features: nil,
+					},
+				},
+			}),
+			target:    &iotago.Transaction{},
+			seriErr:   iotago.ErrNonUniqueChainOutputs,
+			deSeriErr: nil,
+		},
+		{
+			// we transition the same Foundry twice
+			name: "transition the same Foundry twice",
+			source: tpkg.RandTransactionWithEssence(&iotago.TransactionEssence{
+				NetworkID: tpkg.TestNetworkID,
+				Inputs:    inputIDs.UTXOInputs(),
+				Outputs: iotago.TxEssenceOutputs{
+					&iotago.AliasOutput{
+						Amount:  OneMi,
+						AliasID: aliasID,
+						Conditions: iotago.AliasOutputUnlockConditions{
+							&iotago.StateControllerAddressUnlockCondition{Address: ident1},
+							&iotago.GovernorAddressUnlockCondition{Address: ident1},
+						},
+						Features: nil,
+					},
+					&iotago.FoundryOutput{
+						Amount:       OneMi,
+						NativeTokens: nil,
+						SerialNumber: 1,
+						TokenScheme: &iotago.SimpleTokenScheme{
+							MintedTokens:  big.NewInt(50),
+							MeltedTokens:  big.NewInt(0),
+							MaximumSupply: big.NewInt(50),
+						},
+						Conditions: iotago.FoundryOutputUnlockConditions{
+							&iotago.ImmutableAliasUnlockCondition{Address: &aliasAddress},
+						},
+						Features: nil,
+					},
+					&iotago.FoundryOutput{
+						Amount:       OneMi,
+						NativeTokens: nil,
+						SerialNumber: 1,
+						TokenScheme: &iotago.SimpleTokenScheme{
+							MintedTokens:  big.NewInt(50),
+							MeltedTokens:  big.NewInt(0),
+							MaximumSupply: big.NewInt(50),
+						},
+						Conditions: iotago.FoundryOutputUnlockConditions{
+							&iotago.ImmutableAliasUnlockCondition{Address: &aliasAddress},
 						},
 						Features: nil,
 					},
