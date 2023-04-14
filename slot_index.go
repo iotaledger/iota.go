764722433131e582be15be5516027ac87cbd7ffe
Andrea V <1577639+karimodm@users.noreply.github.com>
2023-04-14 14:07:07 +0200

    Remove Max from SlotIndex

--- slot_index.go
+++ slot_index.go
@@ -31,15 +31,6 @@ func (i SlotIndex) String() string {
 	return fmt.Sprintf("SlotIndex(%d)", i)
 }
 
-// Max returns the maximum of the two given slots.
-func (i SlotIndex) Max(other SlotIndex) SlotIndex {
-	if i > other {
-		return i
-	}
-
-	return other
-}
-
 // Abs returns the absolute value of the SlotIndex.
 func (i SlotIndex) Abs() (absolute SlotIndex) {
 	if i < 0 {
