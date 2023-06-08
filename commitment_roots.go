f63351606bcbc20d4800ac936563e1b30041b6d7
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-06-08 12:07:27 +0800

    Update commitment roots to include attestations in merkle root computation and to provide a proof verification for it

--- commitment_roots.go
+++ commitment_roots.go
@@ -9,26 +9,49 @@ import (
 type Roots struct {
 	TangleRoot        Identifier `serix:"0"`
 	StateMutationRoot Identifier `serix:"1"`
-	ActivityRoot      Identifier `serix:"2"`
-	StateRoot         Identifier `serix:"3"`
-	ManaRoot          Identifier `serix:"4"`
+	StateRoot         Identifier `serix:"2"`
+	ManaRoot          Identifier `serix:"3"`
+	AttestationsRoot  Identifier `serix:"4"`
 }
 
 func NewRoots(tangleRoot, stateMutationRoot, activityRoot, stateRoot, manaRoot Identifier) *Roots {
 	return &Roots{
 		TangleRoot:        tangleRoot,
 		StateMutationRoot: stateMutationRoot,
-		ActivityRoot:      activityRoot,
+		AttestationsRoot:  activityRoot,
 		StateRoot:         stateRoot,
 		ManaRoot:          manaRoot,
 	}
 }
 
 func (r *Roots) ID() (id Identifier) {
-	branch1Hashed := blake2b.Sum256(byteutils.ConcatBytes(r.TangleRoot[:], r.StateMutationRoot[:]))
-	branch2Hashed := blake2b.Sum256(byteutils.ConcatBytes(r.StateRoot[:], r.ManaRoot[:]))
-	rootHashed := blake2b.Sum256(byteutils.ConcatBytes(branch1Hashed[:], branch2Hashed[:]))
+	h01 := blake2b.Sum256(byteutils.ConcatBytes(r.TangleRoot[:], r.StateMutationRoot[:]))
+	h23 := blake2b.Sum256(byteutils.ConcatBytes(r.StateRoot[:], r.ManaRoot[:]))
+	h0123 := blake2b.Sum256(byteutils.ConcatBytes(h01[:], h23[:]))
 
-	//TODO: hash ActivityRoot?
+	h45 := blake2b.Sum256(byteutils.ConcatBytes(r.AttestationsRoot[:], emptyIdentifier[:]))
+	h67 := blake2b.Sum256(byteutils.ConcatBytes(emptyIdentifier[:], emptyIdentifier[:]))
+	h4567 := blake2b.Sum256(byteutils.ConcatBytes(h45[:], h67[:]))
+
+	rootHashed := blake2b.Sum256(byteutils.ConcatBytes(h0123[:], h4567[:]))
+
 	return rootHashed
+}
+
+func (r *Roots) AttestationsProof() Identifier {
+	h01 := blake2b.Sum256(byteutils.ConcatBytes(r.TangleRoot[:], r.StateMutationRoot[:]))
+	h23 := blake2b.Sum256(byteutils.ConcatBytes(r.StateRoot[:], r.ManaRoot[:]))
+	h0123 := blake2b.Sum256(byteutils.ConcatBytes(h01[:], h23[:]))
+
+	return h0123
+}
+
+func VerifyRootsAttestationsProof(rootsID, h0123, attestationsRoot Identifier) bool {
+	h45 := blake2b.Sum256(byteutils.ConcatBytes(attestationsRoot[:], emptyIdentifier[:]))
+	h67 := blake2b.Sum256(byteutils.ConcatBytes(emptyIdentifier[:], emptyIdentifier[:]))
+	h4567 := blake2b.Sum256(byteutils.ConcatBytes(h45[:], h67[:]))
+
+	computedRootsID := blake2b.Sum256(byteutils.ConcatBytes(h0123[:], h4567[:]))
+
+	return rootsID == computedRootsID
 }
