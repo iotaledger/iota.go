3310a33d2380caa8197ad9f7b3706a418d4c5f1b
muXxer <git@muxxer.de>
2023-07-20 10:22:16 +0200

    Remove httpOutput

--- nodeclient/apimodels/rest_api.go
+++ nodeclient/apimodels/rest_api.go
@@ -8,24 +8,6 @@ import (
 var _internalAPI = iotago.V3API(iotago.NewV3ProtocolParameters())
 
 type (
-	httpOutput interface{ iotago.Output }
-)
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func init() {
-	api := _internalAPI.Underlying()
-	must(api.RegisterInterfaceObjects((*httpOutput)(nil), (*iotago.BasicOutput)(nil)))
-	must(api.RegisterInterfaceObjects((*httpOutput)(nil), (*iotago.AccountOutput)(nil)))
-	must(api.RegisterInterfaceObjects((*httpOutput)(nil), (*iotago.FoundryOutput)(nil)))
-	must(api.RegisterInterfaceObjects((*httpOutput)(nil), (*iotago.NFTOutput)(nil)))
-}
-
-type (
 	// RoutesResponse defines the response of a GET routes REST API call.
 	RoutesResponse struct {
 		Routes []string `json:"routes"`
