d3ff3bb45be8d1388d409505d0a3f8dbd865cda2
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-08-25 15:29:15 +0200

    Move VersionAndHash from iota-core

--- api.go
+++ api.go
@@ -7,6 +7,8 @@ import (
 	"golang.org/x/crypto/blake2b"
 
 	"github.com/iotaledger/hive.go/ierrors"
+	"github.com/iotaledger/hive.go/lo"
+	"github.com/iotaledger/hive.go/serializer/v2/byteutils"
 	"github.com/iotaledger/hive.go/serializer/v2/serix"
 )
 
@@ -22,6 +24,30 @@ func VersionFromBytes(b []byte) (Version, int, error) {
 	}
 
 	return Version(b[0]), 1, nil
+}
+
+type VersionAndHash struct {
+	Version Version
+	Hash    Identifier
+}
+
+func (v VersionAndHash) Bytes() ([]byte, error) {
+	// iotago.Version and iotago.Identifier can't panic on .Bytes() call.
+	return byteutils.ConcatBytes(lo.PanicOnErr(v.Version.Bytes()), lo.PanicOnErr(v.Hash.Bytes())), nil
+}
+
+func VersionAndHashFromBytes(bytes []byte) (VersionAndHash, int, error) {
+	version, versionBytesConsumed, err := VersionFromBytes(bytes)
+	if err != nil {
+		return VersionAndHash{}, 0, ierrors.Wrap(err, "failed to parse version")
+	}
+
+	hash, hashBytesConsumed, err := IdentifierFromBytes(bytes[versionBytesConsumed:])
+	if err != nil {
+		return VersionAndHash{}, 0, ierrors.Wrap(err, "failed to parse hash")
+	}
+
+	return VersionAndHash{version, hash}, versionBytesConsumed + hashBytesConsumed, nil
 }
 
 type VersionSignaling struct {
