8d109aa8426a24112feb3c51e56150ae0506f5a6
jkrvivian <jkrvivian@gmail.com>
2023-09-07 14:30:16 +0800

    Rename to bitsCount

--- vm/stardust/vm_stardust_test.go
+++ vm/stardust/vm_stardust_test.go
@@ -21,7 +21,7 @@ const (
 	betaPerYear                  float64 = 1 / 3.0
 	slotsPerEpochExponent                = 13
 	slotDurationSeconds                  = 10
-	bitsExponent                         = 63
+	bitsCount                            = 63
 	generationRate                       = 1
 	generationRateExponent               = 27
 	decayFactorsExponent                 = 32
@@ -37,7 +37,7 @@ var (
 		iotago.WithSupplyOptions(tpkg.TestTokenSupply, 100, 1, 10, 100, 100, 100),
 		iotago.WithWorkScoreOptions(1, 100, 500, 20, 20, 20, 20, 100, 100, 100, 200, 4),
 		iotago.WithTimeProviderOptions(100, slotDurationSeconds, slotsPerEpochExponent),
-		iotago.WithManaOptions(bitsExponent,
+		iotago.WithManaOptions(bitsCount,
 			generationRate,
 			generationRateExponent,
 			tpkg.ManaDecayFactors(betaPerYear, 1<<slotsPerEpochExponent, slotDurationSeconds, decayFactorsExponent),
