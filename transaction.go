b88a0b949aa9febfdc23d91668d80087ab70c9bf
muXxer <git@muxxer.de>
2023-09-25 17:25:11 +0200

    ReAdd TransactionIDLength

--- transaction.go
+++ transaction.go
@@ -7,6 +7,11 @@ import (
 	"github.com/iotaledger/hive.go/serializer/v2"
 )
 
+const (
+	// TransactionIDLength defines the length of a Transaction ID.
+	TransactionIDLength = SlotIdentifierLength
+)
+
 var (
 	// ErrMissingUTXO gets returned if an UTXO is missing to commence a certain operation.
 	ErrMissingUTXO = ierrors.New("missing utxo")
