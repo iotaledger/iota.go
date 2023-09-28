0d523cd00d0e4f161e52c676da52c88bf03c3e08
muXxer <git@muxxer.de>
2023-09-28 13:39:11 +0200

    Remove workscore parameters for missing strong parents

--- tpkg/util.go
+++ tpkg/util.go
@@ -943,18 +943,16 @@ func RandWorkScore(max uint32) iotago.WorkScore {
 // RandWorkscoreStructure produces random workscore structure.
 func RandWorkscoreStructure() *iotago.WorkScoreStructure {
 	return &iotago.WorkScoreStructure{
-		DataByte:                  RandWorkScore(math.MaxUint32),
-		Block:                     RandWorkScore(math.MaxUint32),
-		MissingParent:             RandWorkScore(math.MaxUint32),
-		Input:                     RandWorkScore(math.MaxUint32),
-		ContextInput:              RandWorkScore(math.MaxUint32),
-		Output:                    RandWorkScore(math.MaxUint32),
-		NativeToken:               RandWorkScore(math.MaxUint32),
-		Staking:                   RandWorkScore(math.MaxUint32),
-		BlockIssuer:               RandWorkScore(math.MaxUint32),
-		Allotment:                 RandWorkScore(math.MaxUint32),
-		SignatureEd25519:          RandWorkScore(math.MaxUint32),
-		MinStrongParentsThreshold: RandUint8(math.MaxUint8),
+		DataByte:         RandWorkScore(math.MaxUint32),
+		Block:            RandWorkScore(math.MaxUint32),
+		Input:            RandWorkScore(math.MaxUint32),
+		ContextInput:     RandWorkScore(math.MaxUint32),
+		Output:           RandWorkScore(math.MaxUint32),
+		NativeToken:      RandWorkScore(math.MaxUint32),
+		Staking:          RandWorkScore(math.MaxUint32),
+		BlockIssuer:      RandWorkScore(math.MaxUint32),
+		Allotment:        RandWorkScore(math.MaxUint32),
+		SignatureEd25519: RandWorkScore(math.MaxUint32),
 	}
 }
 
@@ -985,8 +983,6 @@ func RandProtocolParameters() iotago.ProtocolParameters {
 			RandWorkScore(math.MaxUint32),
 			RandWorkScore(math.MaxUint32),
 			RandWorkScore(math.MaxUint32),
-			RandWorkScore(math.MaxUint32),
-			RandByte(),
 		),
 		iotago.WithTimeProviderOptions(time.Now().Unix(), RandUint8(math.MaxUint8), RandUint8(math.MaxUint8)),
 		iotago.WithLivenessOptions(RandUint16(math.MaxUint16), RandUint16(math.MaxUint16), RandSlotIndex(), RandSlotIndex(), RandSlotIndex()),
