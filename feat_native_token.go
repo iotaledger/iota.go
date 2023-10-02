d513bba234a8a3175bcde64297d5ad7837226716
muXxer <git@muxxer.de>
2023-10-02 13:57:53 +0200

    Apply suggestions from code review
    Co-authored-by: Philipp Gackstatter <philipp.gackstatter@iota.org>

--- feat_native_token.go
+++ feat_native_token.go
@@ -17,13 +17,13 @@ const (
 
 var (
 	// ErrNativeTokenAmountLessThanEqualZero gets returned when a NativeToken.Amount is not bigger than 0.
-	ErrNativeTokenAmountLessThanEqualZero = ierrors.New("native token must be a value bigger than zero")
+	ErrNativeTokenAmountLessThanEqualZero = ierrors.New("native token amount must be greater than zero")
 	// ErrNativeTokenSumExceedsUint256 gets returned when a NativeToken.Amount addition results in a value bigger than the max value of a uint256.
 	ErrNativeTokenSumExceedsUint256 = ierrors.New("native token sum exceeds max value of a uint256")
 	// ErrNativeTokenSumUnbalanced gets returned when two NativeTokenSum(s) are unbalanced.
 	ErrNativeTokenSumUnbalanced = ierrors.New("native token sums are unbalanced")
 	// ErrFoundryIDNativeTokenIDMismatch gets returned when a native token features exists in a foundry output but the IDs mismatch.
-	ErrFoundryIDNativeTokenIDMismatch = ierrors.New("native token feature ID in foundry output must match the foundry ID")
+	ErrFoundryIDNativeTokenIDMismatch = ierrors.New("native token ID in foundry output must match the foundry ID")
 )
 
 // NativeTokenID is an identifier which uniquely identifies a NativeToken.
