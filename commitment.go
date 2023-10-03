4d0ec83cd828cf899342b281879bd89d8db9e803
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-10-03 11:26:17 +0200

    Cleanup code

--- commitment.go
+++ commitment.go
@@ -52,16 +52,6 @@ func (c *Commitment) ID() (CommitmentID, error) {
 	return SlotIdentifierRepresentingData(c.Slot, data), nil
 }
 
-// OutputID returns a virtual OutputID that is used to address the commitment. C
-func (c *Commitment) OutputID() (outputID OutputID, err error) {
-	commitmentID, err := c.ID()
-	if err != nil {
-		return outputID, ierrors.Wrap(err, "failed to compute commitment ID")
-	}
-
-	return NewOutputID(commitmentID, 0), nil
-}
-
 func (c *Commitment) StateID() Identifier {
 	return IdentifierFromData(lo.PanicOnErr(c.MustID().Bytes()))
 }
