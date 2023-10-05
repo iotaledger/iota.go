2f6977a17709fa553cefeb209ee18cc2c3bf6a03
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-10-05 13:26:12 +0200

    Generate TransactionID from template and fix all incompatibilities

--- gen/slot_identifier_gen.go
+++ gen/slot_identifier_gen.go
@@ -4,3 +4,4 @@ package gen
 
 //go:generate go run ./cmd slot_identifier.tmpl ../block_id.gen.go BlockID b "ids"
 //go:generate go run ./cmd slot_identifier.tmpl ../commitment_id.gen.go CommitmentID c ""
+//go:generate go run ./cmd slot_identifier.tmpl ../transaction_id.gen.go TransactionID t "ids"
