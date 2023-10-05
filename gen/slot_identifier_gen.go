62e6761a7b7ceb6b0e7b3bc0df7461327f77cea5
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-10-05 21:45:34 +0300

    Add ids feature to CommitmentID

--- gen/slot_identifier_gen.go
+++ gen/slot_identifier_gen.go
@@ -3,7 +3,7 @@
 package gen
 
 //go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@86973b2edb3b slot_identifier.tmpl ../block_id.gen.go BlockID b "ids"
-//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@86973b2edb3b slot_identifier.tmpl ../commitment_id.gen.go CommitmentID c ""
+//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@86973b2edb3b slot_identifier.tmpl ../commitment_id.gen.go CommitmentID c "ids"
 //go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@86973b2edb3b slot_identifier.tmpl ../transaction_id.gen.go TransactionID t "ids"
 //go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@86973b2edb3b slot_identifier.tmpl ../signed_transaction_id.gen.go SignedTransactionID t "ids"
 //go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@86973b2edb3b slot_identifier.tmpl ../output_id.gen.go OutputID o "ids output"
