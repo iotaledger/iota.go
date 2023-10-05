bcf1805224144fb1ca97061e0b425d2076936bd4
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-10-05 13:28:00 +0200

    Generate SignedTransactionID from template and fix all incompatibilities

--- gen/slot_identifier_gen.go
+++ gen/slot_identifier_gen.go
@@ -5,3 +5,4 @@ package gen
 //go:generate go run ./cmd slot_identifier.tmpl ../block_id.gen.go BlockID b "ids"
 //go:generate go run ./cmd slot_identifier.tmpl ../commitment_id.gen.go CommitmentID c ""
 //go:generate go run ./cmd slot_identifier.tmpl ../transaction_id.gen.go TransactionID t "ids"
+//go:generate go run ./cmd slot_identifier.tmpl ../signed_transaction_id.gen.go SignedTransactionID t "ids"
