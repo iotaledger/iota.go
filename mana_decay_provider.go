e5f14459318fa5afcb61d2e1f434d09e50756b2e
oliviasaa <olivia.saa@iota.org>
2023-10-19 13:23:49 +0100

    Update mana_decay_provider.go

--- mana_decay_provider.go
+++ mana_decay_provider.go
@@ -1,57 +1,13 @@
 package iotago
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/iotaledger/hive.go/core/safemath"
 	"github.com/iotaledger/hive.go/ierrors"
 	"github.com/iotaledger/hive.go/lo"
 )
-
-// splitUint64 splits a uint64 value into two uint64 that hold the high and the low double-word.
-func splitUint64(value uint64) (valueHi uint64, valueLo uint64) {
-	return value >> 32, value & 0x00000000FFFFFFFF
-}
-
-// mergeUint64 merges two uint64 values that hold the high and the low double-word into one uint64.
-func mergeUint64(valueHi uint64, valueLo uint64) (value uint64) {
-	return (valueHi << 32) | valueLo
-}
-
-// fixedPointMultiplication32Splitted does a fixed point multiplication using two uint64
-// containing the high and the low double-word of the value.
-// ATTENTION: do not pass factor that use more than 32bits, otherwise this function overflows.
-func fixedPointMultiplication32Splitted(valueHi uint64, valueLo uint64, factor uint64, scale uint64) (uint64, uint64) {
-	// multiply the integer part of the fixed-point number by the factor
-	valueHi *= factor
-
-	// the lower 'scale' bits of the result are extracted and shifted left to form the lower part of the new fraction.
-	// the fractional part of the fixed-point number is multiplied by the factor and right-shifted by 'scale' bits.
-	// the sum of these two values forms the new lower part (valueLo) of the result.
-	valueLo = (valueHi&((1<<scale)-1))<<(32-scale) + (valueLo*factor)>>scale
-
-	// the right-shifted valueHi and the upper 32 bits of valueLo form the new higher part (valueHi) of the result.
-	valueHi = (valueHi >> scale) + (valueLo >> 32)
-
-	// the lower 32 bits of valueLo form the new lower part of the result.
-	valueLo &= 0x00000000FFFFFFFF
-
-	// return the result as a fixed-point number composed of two 64-bit integers
-	return valueHi, valueLo
-}
-
-// fixedPointMultiplication32 does a fixed point multiplication.
-// ATTENTION: do not pass factor that use more than 32bits, otherwise this function overflows.
-func fixedPointMultiplication32(value uint64, factor uint64, scale uint64) uint64 {
-	extraShift := uint64(0)
-	if scale > 32 {
-		extraShift = scale - 32
-		scale = 32
-	}
-	valueHi, valueLo := splitUint64(value)
-
-	return mergeUint64(fixedPointMultiplication32Splitted(valueHi, valueLo, factor, scale)) >> extraShift
-}
 
 // ManaDecayProvider calculates the mana decay and mana generation
 // using fixed point arithmetic and a precomputed lookup table.
@@ -113,9 +69,6 @@ func (p *ManaDecayProvider) decay(value Mana, epochDiff EpochIndex) Mana {
 		return value
 	}
 
-	// split the value into two uint64 variables to prevent overflows
-	valueHi, valueLo := splitUint64(uint64(value))
-
 	// we keep applying the decay as long as epoch index diffs are left
 	remainingEpochDiff := epochDiff
 	for remainingEpochDiff > 0 {
@@ -131,11 +84,12 @@ func (p *ManaDecayProvider) decay(value Mana, epochDiff EpochIndex) Mana {
 		decayFactor := p.decayFactors[diffsToDecay-1]
 
 		// apply the decay and scale the resulting value (fixed-point arithmetics)
-		valueHi, valueLo = fixedPointMultiplication32Splitted(valueHi, valueLo, decayFactor, p.decayFactorsExponent)
+		aux, _ := safemath.Safe64MulShift(uint64(value), decayFactor, p.decayFactorsExponent)
+		value = Mana(aux)
 	}
 
 	// combine both uint64 variables to get the actual value
-	return Mana(mergeUint64(valueHi, valueLo))
+	return value
 }
 
 // generateMana calculates the generated mana.
@@ -143,8 +97,8 @@ func (p *ManaDecayProvider) generateMana(value BaseToken, slotDiff SlotIndex) Ma
 	if slotDiff == 0 || p.generationRate == 0 {
 		return 0
 	}
-
-	return Mana(fixedPointMultiplication32(uint64(value), uint64(slotDiff)*p.generationRate, p.generationRateExponent))
+	aux, _ := safemath.Safe64MulShift(uint64(value), uint64(slotDiff)*p.generationRate, p.generationRateExponent)
+	return Mana(aux)
 }
 
 // ManaWithDecay applies the decay to the given mana.
@@ -181,8 +135,8 @@ func (p *ManaDecayProvider) ManaGenerationWithDecay(amount BaseToken, creationSl
 		return safemath.SafeAdd(manaDecayed, manaGenerated)
 
 	default:
-		c := Mana(fixedPointMultiplication32(uint64(amount), p.decayFactorEpochsSum*p.generationRate, p.decayFactorEpochsSumExponent+p.generationRateExponent-p.slotsPerEpochExponent))
-
+		aux, _ := safemath.Safe64MulShift(uint64(amount), p.decayFactorEpochsSum*p.generationRate, p.decayFactorEpochsSumExponent+p.generationRateExponent-p.slotsPerEpochExponent)
+		c := Mana(aux)
 		//nolint:golint,revive,nosnakecase,stylecheck // taken from the formula, lets keep it that way
 		potentialMana_n := p.decay(p.generateMana(amount, p.timeProvider.SlotsBeforeNextEpoch(creationSlot)), epochDiff)
 
@@ -206,7 +160,9 @@ func (p *ManaDecayProvider) ManaGenerationWithDecay(amount BaseToken, creationSl
 		if err != nil {
 			return 0, err
 		}
+		printVar, _ := safemath.SafeAdd(result, potentialMana_n)
 		//nolint:golint,revive,nosnakecase,stylecheck // taken from the formula, lets keep it that way
+		fmt.Println(p.LowerBoundPotentialMana(amount, creationSlot, targetSlot), printVar, p.UpperBoundPotentialMana(amount, creationSlot, targetSlot))
 		return safemath.SafeAdd(result, potentialMana_n)
 	}
 }
