52f33b838f6e08cda54d3dcba1a9ec50f96c25f0
muXxer <git@muxxer.de>
2023-10-20 14:32:59 +0200

    Auto-generate boilerplate code for identifier types

--- gen/identifier_gen.go
+++ gen/identifier_gen.go
@@ -2,5 +2,5 @@
 
 package gen
 
-//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@86973b2edb3b identifier.tmpl ../identifier.gen.go Identifier i ""
-//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@86973b2edb3b identifier.tmpl ../identifier_account.gen.go AccountID a ""
+//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@13da292 identifier.tmpl ../identifier.gen.go Identifier i "" ""
+//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@13da292 identifier.tmpl ../identifier_account.gen.go AccountID a "chainid" "AddressType=AccountAddress"
