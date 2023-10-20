eedb3531542615f5b238bdbb4da9bfebb7692f41
muXxer <git@muxxer.de>
2023-10-20 16:08:14 +0200

    Auto-generate boilerplate code for reference unlock types

---
+++ gen/unlock_ref_gen.go
@@ -0,0 +1,7 @@
+//go:build ignore
+
+package gen
+
+//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@13da292 unlock_ref.tmpl ../unlock_reference.gen.go ReferenceUnlock r "" "SourceAddressType=ChainAddress,UnlockType=UnlockReference"
+//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@13da292 unlock_ref.tmpl ../unlock_account.gen.go AccountUnlock r "chainable" "SourceAddressType=*AccountAddress,UnlockType=UnlockAccount"
+//go:generate go run github.com/iotaledger/hive.go/codegen/features/cmd@13da292 unlock_ref.tmpl ../unlock_nft.gen.go NFTUnlock r "chainable" "SourceAddressType=*NFTAddress,UnlockType=UnlockNFT"
