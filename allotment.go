bd9f02535dd015cbb795c11f7e464403dd615da7
Philipp Gackstatter <philipp.gackstatter@iota.org>
2024-01-18 16:21:33 +0100

    Remove unused Allotment Equal func

--- allotment.go
+++ allotment.go
@@ -27,10 +27,6 @@ func (a *Allotment) Clone() *Allotment {
 	}
 }
 
-func (a *Allotment) Equal(other *Allotment) bool {
-	return a.AccountID == other.AccountID && a.Mana == other.Mana
-}
-
 func (a *Allotment) Compare(other *Allotment) int {
 	return bytes.Compare(a.AccountID[:], other.AccountID[:])
 }
@@ -40,20 +36,6 @@ type Allotments []*Allotment
 
 func (a Allotments) Clone() Allotments {
 	return lo.CloneSlice(a)
-}
-
-func (a Allotments) Equal(other Allotments) bool {
-	if len(a) != len(other) {
-		return false
-	}
-
-	for idx, allotment := range a {
-		if !allotment.Equal(other[idx]) {
-			return false
-		}
-	}
-
-	return true
 }
 
 // Sort sorts the allotments in lexical order.
