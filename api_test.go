2ca9908c085baabb6b7dbb994f0ba9df48d2d467
muXxer <git@muxxer.de>
2024-01-23 14:41:40 +0100

    Reintroduce nicer version of `TestProtocolParametersJSONMarshalling`

--- api_test.go
+++ api_test.go
@@ -2,6 +2,8 @@
 package iotago_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"reflect"
 	"testing"
 
@@ -104,5 +106,131 @@ func TestProtocolParameters_DeSerialize(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, tt.deSerialize)
+	}
+}
+
+// jsonEncodeTest is used to check if the JSON encoding is equal to a manually provided JSON string.
+type jsonEncodeTest struct {
+	name   string
+	source any
+	// the target should be an indented JSON string (tabs instead of spaces)
+	target string
+}
+
+func (test *jsonEncodeTest) run(t *testing.T) {
+	t.Helper()
+
+	sourceJSON, err := tpkg.ZeroCostTestAPI.JSONEncode(test.source, serix.WithValidation())
+	require.NoError(t, err, "JSON encoding")
+
+	var b bytes.Buffer
+	err = json.Indent(&b, sourceJSON, "", "\t")
+	require.NoError(t, err, "JSON indenting")
+	indentedJSON := b.String()
+
+	require.EqualValues(t, test.target, string(indentedJSON))
+}
+
+func TestProtocolParametersJSONMarshalling(t *testing.T) {
+
+	protoParams := iotago.NewV3SnapshotProtocolParameters(
+		iotago.WithTimeProviderOptions(1, 1690879505, 10, 13),
+	)
+
+	// replace the decay factors to reduce the size of the JSON string
+	protoParams.ManaParameters().DecayFactors = []uint32{
+		1,
+		2,
+		3,
+	}
+
+	tests := []*jsonEncodeTest{
+		{
+			name:   "ok",
+			source: protoParams,
+			target: `{
+	"type": 0,
+	"version": 3,
+	"networkName": "testnet",
+	"bech32Hrp": "rms",
+	"storageScoreParameters": {
+		"storageCost": "100",
+		"factorData": 1,
+		"offsetOutputOverhead": "10",
+		"offsetEd25519BlockIssuerKey": "100",
+		"offsetStakingFeature": "100",
+		"offsetDelegation": "100"
+	},
+	"workScoreParameters": {
+		"dataByte": 1,
+		"block": 100,
+		"input": 10,
+		"contextInput": 20,
+		"output": 20,
+		"nativeToken": 20,
+		"staking": 5000,
+		"blockIssuer": 1000,
+		"allotment": 1000,
+		"signatureEd25519": 1000
+	},
+	"manaParameters": {
+		"bitsCount": 63,
+		"generationRate": 1,
+		"generationRateExponent": 17,
+		"decayFactors": [
+			1,
+			2,
+			3
+		],
+		"decayFactorsExponent": 32,
+		"decayFactorEpochsSum": 2262417561,
+		"decayFactorEpochsSumExponent": 21,
+		"annualDecayFactorPercentage": 70
+	},
+	"tokenSupply": "1813620509061365",
+	"genesisSlot": 1,
+	"genesisUnixTimestamp": "1690879505",
+	"slotDurationInSeconds": 10,
+	"slotsPerEpochExponent": 13,
+	"stakingUnbondingPeriod": 10,
+	"validationBlocksPerSlot": 10,
+	"punishmentEpochs": 10,
+	"livenessThresholdLowerBound": 15,
+	"livenessThresholdUpperBound": 30,
+	"minCommittableAge": 10,
+	"maxCommittableAge": 20,
+	"epochNearingThreshold": 60,
+	"congestionControlParameters": {
+		"minReferenceManaCost": "1",
+		"increase": "10",
+		"decrease": "10",
+		"increaseThreshold": 800000,
+		"decreaseThreshold": 500000,
+		"schedulerRate": 100000,
+		"maxBufferSize": 1000,
+		"maxValidationBufferSize": 100
+	},
+	"versionSignalingParameters": {
+		"windowSize": 7,
+		"windowTargetRatio": 5,
+		"activationOffset": 7
+	},
+	"rewardsParameters": {
+		"profitMarginExponent": 8,
+		"bootstrappingDuration": 1079,
+		"manaShareCoefficient": "2",
+		"decayBalancingConstantExponent": 8,
+		"decayBalancingConstant": "1",
+		"poolCoefficientExponent": 11,
+		"retentionPeriod": 384
+	},
+	"targetCommitteeSize": 32,
+	"chainSwitchingThreshold": 3
+}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, tt.run)
 	}
 }
