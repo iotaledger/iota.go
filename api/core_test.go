fe6bda06648e2432e7fa6e67311d57ea0e105c47
muXxer <git@muxxer.de>
2024-02-22 10:26:20 +0100

    Add `EarliestAttachmentSlot` to `TransactionMetadataResponse`

--- api/core_test.go
+++ api/core_test.go
@@ -540,12 +540,14 @@ func Test_CoreAPIJSONSerialization(t *testing.T) {
 			Source: &api.TransactionMetadataResponse{
 				TransactionID:             iotago.TransactionID{0x1},
 				TransactionState:          api.TransactionStateFailed,
+				EarliestAttachmentSlot:    5,
 				TransactionFailureReason:  api.TxFailureDelegationRewardsClaimingInvalid,
 				TransactionFailureDetails: "details",
 			},
 			Target: `{
 	"transactionId": "0x010000000000000000000000000000000000000000000000000000000000000000000000",
 	"transactionState": "failed",
+	"earliestAttachmentSlot": 5,
 	"transactionFailureReason": 57,
 	"transactionFailureDetails": "details"
 }`,
@@ -553,12 +555,14 @@ func Test_CoreAPIJSONSerialization(t *testing.T) {
 		{
 			Name: "ok - TransactionMetadataResponse - omitempty",
 			Source: &api.TransactionMetadataResponse{
-				TransactionID:    iotago.TransactionID{0x1},
-				TransactionState: api.TransactionStateConfirmed,
+				TransactionID:          iotago.TransactionID{0x1},
+				TransactionState:       api.TransactionStateConfirmed,
+				EarliestAttachmentSlot: 10,
 			},
 			Target: `{
 	"transactionId": "0x010000000000000000000000000000000000000000000000000000000000000000000000",
-	"transactionState": "confirmed"
+	"transactionState": "confirmed",
+	"earliestAttachmentSlot": 10
 }`,
 		},
 		{
