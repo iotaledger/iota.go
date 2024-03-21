4e586367e5bd06688f170b8c3aab03734dfef6a7
muXxer <git@muxxer.de>
2024-03-21 18:44:45 +0100

    Cleanup management API

--- api/management.go
+++ api/management.go
@@ -21,70 +21,24 @@ type (
 		MultiAddresses []iotago.PrefixedStringUint8 `serix:",lenPrefix=uint8"`
 		// The alias to identify the peer.
 		Alias string `serix:",lenPrefix=uint8,omitempty"`
-		// The relation (static, autopeered) of the peer.
+		// The relation (manual, autopeered) of the peer.
 		Relation string `serix:",lenPrefix=uint8"`
 		// Whether the peer is connected.
 		Connected bool `serix:""`
-		// The gossip related information about this peer.
-		Gossip *GossipInfo `serix:",optional,omitempty"`
-	}
-
-	PeersResponse struct {
-		Peers []*PeerInfo `serix:",lenPrefix=uint8"`
-	}
-
-	// GossipInfo represents information about an ongoing gossip protocol.
-	GossipInfo struct {
-		// The last received heartbeat by the given node.
-		Heartbeat *GossipHeartbeat `serix:",omitempty"`
-		// The metrics about sent and received protocol messages.
-		Metrics *PeerGossipMetrics `serix:",omitempty"`
-	}
-
-	// GossipHeartbeat represents a gossip heartbeat message.
-	// Peers send each other this gossip protocol message when their
-	// state is updated, such as when:
-	//	- a new slot was received
-	//	- the solid slot changed
-	//	- the node performed pruning of data
-	GossipHeartbeat struct {
-		// The solid slot of the node.
-		SolidSlot iotago.SlotIndex `serix:""`
-		// The oldest known slot by the node.
-		PrunedSlot iotago.SlotIndex `serix:""`
-		// The latest known slot by the node.
-		LatestSlot iotago.SlotIndex `serix:""`
-		// The amount of currently connected peers.
-		ConnectedPeers uint32 `serix:""`
-		// The amount of currently connected peers who also
-		// are synchronized with the network.
-		SyncedPeers uint32 `serix:""`
+		// The gossip metrics for this peer.
+		GossipMetrics *PeerGossipMetrics `serix:""`
 	}
 
 	// PeerGossipMetrics defines the peer gossip metrics.
 	PeerGossipMetrics struct {
-		// The total amount of received new blocks.
-		NewBlocks uint32 `serix:""`
-		// The total amount of received known blocks.
-		KnownBlocks uint32 `serix:""`
-		// The total amount of received blocks.
-		ReceivedBlocks uint32 `serix:""`
-		// The total amount of received block requests.
-		ReceivedBlockRequests uint32 `serix:""`
-		// The total amount of received slot requests.
-		ReceivedSlotRequests uint32 `serix:""`
-		// The total amount of received heartbeats.
-		ReceivedHeartbeats uint32 `serix:""`
-		// The total amount of sent blocks.
-		SentBlocks uint32 `serix:""`
-		// The total amount of sent block request.
-		SentBlockRequests uint32 `serix:""`
-		// The total amount of sent slot request.
-		SentSlotRequests uint32 `serix:""`
-		// The total amount of sent heartbeats.
-		SentHeartbeats uint32 `serix:""`
-		// The total amount of packets which couldn't be sent.
-		DroppedPackets uint32 `serix:""`
+		// The total amount of received packets.
+		PacketsReceived uint32 `serix:""`
+		// The total amount of sent packets.
+		PacketsSent uint32 `serix:""`
+	}
+
+	PeersResponse struct {
+		Peers []*PeerInfo `serix:",lenPrefix=uint8"`
 	}
 
 	// PruneDatabaseRequest defines the request of a prune database REST API call.
@@ -101,19 +55,5 @@ type (
 	PruneDatabaseResponse struct {
 		// The current oldest epoch in the database.
 		Epoch iotago.EpochIndex `serix:""`
-	}
-
-	// CreateSnapshotsRequest defines the request of a create snapshots REST API call.
-	CreateSnapshotsRequest struct {
-		// The slot of the snapshot.
-		Slot iotago.SlotIndex `serix:""`
-	}
-
-	// CreateSnapshotResponse defines the response of a create snapshot REST API call.
-	CreateSnapshotResponse struct {
-		// The slot of the snapshot.
-		Slot iotago.SlotIndex `serix:""`
-		// The file path of the snapshot file.
-		FilePath string `serix:",lenPrefix=uint8"`
 	}
 )
